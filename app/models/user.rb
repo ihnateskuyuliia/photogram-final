71ffe333d62f48e7bdce470eed426a376ed080a2
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-12-03 00:19:16 +0000

    huynya

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id             :integer          not null, primary key
+#  comments_count :integer
+#  likes_count    :integer
+#  private        :boolean
+#  username       :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class User < ApplicationRecord
+#  validates(:username, {
+#    :presence => true,
+#    :uniqueness => { :case_sensitive => false },
+#  })
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+   devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
 end
